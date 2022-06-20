de6df351f62714f8fd92cd9429c10fb1891234a4
Seungwon <dnjs5397@naver.com>
2022-06-20 22:13:56 +0900

    2022/06/20 메인 페이지, 사진 첨부 페이지 제작

---
+++ app/src/main/java/com/swkim/myfish/WorkingActivity.kt
@@ -0,0 +1,11 @@
+package com.swkim.myfish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_working)
+    }
+}
